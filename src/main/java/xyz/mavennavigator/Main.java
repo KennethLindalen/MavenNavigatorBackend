238ab0fd5956de20fa4faedae6abe2772f0b33bd
kenneth <kennethlindalen@gmail.com>
2020-05-02 22:06:28 +0200

    New way of sorting, full of bugs, does not work properly. This is dumb

--- src/main/java/xyz/mavennavigator/Main.java
+++ src/main/java/xyz/mavennavigator/Main.java
@@ -5,7 +5,6 @@ import com.google.gson.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
