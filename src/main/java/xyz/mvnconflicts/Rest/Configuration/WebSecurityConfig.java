919f6608389b068c16b68fb5be7f4aa579a33b95
kenneth <kennethlindalen@gmail.com>
2020-07-31 02:22:39 +0200

    Test with just CSRF disabled

--- src/main/java/xyz/mvnconflicts/Rest/Configuration/WebSecurityConfig.java
+++ src/main/java/xyz/mvnconflicts/Rest/Configuration/WebSecurityConfig.java
@@ -1,9 +1,14 @@
 package xyz.mvnconflicts.Rest.Configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -14,7 +19,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
                 .requiresSecure();
 
-        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
+        http.cors().and().csrf().disable();
+
+//        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
+
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.singletonList("*"));
+        configuration.setAllowedMethods(Collections.singletonList("*"));
+        configuration.setAllowedHeaders(Collections.singletonList("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
